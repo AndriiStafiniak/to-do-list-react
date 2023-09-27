85c6c535e73e067f749031c2e28988f8c58703be
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-09-27 13:20:38 +0200

    added createGlobalStyle

--- src/globalStyled.js
+++ src/globalStyled.js
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
 html {
   box-sizing: border-box;
 }
@@ -12,3 +15,4 @@ body {
   font-family: 'Montserrat', sans-serif;
   background-color: #eee
 }
+`;
