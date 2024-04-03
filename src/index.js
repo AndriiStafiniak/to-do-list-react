0a88131592562e13c378819615d98017a425a631
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-04-03 09:52:38 +0200

    install styled normalize

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { theme } from "./theme"
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './globalStyled';
-import store from "./store"
-import { Provider } from "react-redux"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { theme } from "./theme";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./globalStyled";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Normalize } from "styled-normalize";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
+        <Normalize />
         <App />
       </ThemeProvider>
     </Provider>
